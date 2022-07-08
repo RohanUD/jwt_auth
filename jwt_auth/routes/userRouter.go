05d08c1952efe733c8793a202d43f5f6469331ee
RohanUD <78956997+RohanUD@users.noreply.github.com>
2022-07-08 15:25:45 +0530

    Add files via upload

---
+++ jwt_auth/routes/userRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "last/controllers"
+	"last/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
